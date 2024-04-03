120360367abdaafc1fd9dbb318c041ee1398b3b4
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-03 16:18:28 -0300

    margin, if else

--- script.js
+++ script.js
@@ -17,7 +17,19 @@ function calcularDiasParaAniversario() {
   // Adicionar 1 para considerar que ainda falta um dia completo
   diasParaAniversario += 1;
 
-  // Exibir o resultado
-  let resultado = document.getElementById('diasCalculados');
-  resultado.textContent = "Faltam " + diasParaAniversario + " dias para essa data";
+
+  //exibir resultado
+  if (diasParaAniversario > 0) {
+    let resultado = document.getElementById('diasCalculados');
+    resultado.textContent = "Faltam " + diasParaAniversario + " dias para essa data";
+  } else if (diasParaAniversario === 0) {
+    let resultado = document.getElementById('diasCalculados');
+    resultado.textContent = "Essa data é hoje!";
+  } else {
+    let resultado = document.getElementById('diasCalculados');
+    resultado.textContent = "Já se passaram " + Math.abs(diasParaAniversario) + " dias dessa data";
+  }
 }
+
+
+  
